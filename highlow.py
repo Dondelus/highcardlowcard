c39d774e65d09ade5f448d6aa5f8889d2bbeb261
Cameron Rosier <cameronrosier@hotmail.com>
2019-09-13 20:07:55 -0400

    Refactored game logic to draw a card at the beginning of the game

--- highlow.py
+++ highlow.py
@@ -1,40 +1,69 @@
 import random
 from deck import Deck
 
+
 def play():
+    """
+    Plays a game of high-card low-card
+    """
+
+    # Instantiate a new deck of cards
     deck = Deck(shuffled=True)
-    
+
+    # Hold whether the user has lost or not to determine an 'end-of-game' message
     user_lost = False
+
+    # Draw a card to start the game
+    current_card = deck.next_card()
+
+    # Game will run as long as there are cards left in the deck and the user has not guessed incorrectly
     while deck.length() > 0:
-        current_card = deck.next_card()
         deck.discard_top()
 
-        print("Current card: %s of %s" % (current_card['name'], current_card['suit']))
+        # Print current card and retrieve the user's guess
+        print("Current card: %s of %s" %
+              (current_card['name'], current_card['suit']))
         choice = input("Is the next card higher or lower? (H/L): ")
 
+        # User chose higher
         if choice == 'H':
-            # User chose higher
+
+            # Get next card to compare and print it out
             next_card = deck.next_card()
-            print("Next card: %s of %s" % (next_card['name'], current_card['suit']))
+            print("Next card: %s of %s" %
+                  (next_card['name'], current_card['suit']))
+
+            # Check if the user guessed correctly
             if next_card['real_value'] >= current_card['real_value']:
                 print("You guessed it!\n---------------------------\n")
+                current_card = next_card
             else:
                 print("Incorrect!")
                 user_lost = True
                 break
+
+        # User chose lower
         elif choice == 'L':
-            # User chose lower
+
+            # Get next card to compare and print it out
             next_card = deck.next_card()
-            print("Next card: %s of %s" % (next_card['name'], current_card['suit']))
+            print("Next card: %s of %s" %
+                  (next_card['name'], current_card['suit']))
+
+            # Check if the user guessed correctly
             if next_card['real_value'] <= current_card['real_value']:
                 print("You guessed it!\n---------------------------\n")
+                current_card = next_card
             else:
                 print("Incorrect!")
                 user_lost = True
                 break
+
+        # User entered an incorrect option
         else:
             print("Your options are 'H' for Higher and 'L' for lower.")
-    
+
+    # Print game-over message
     if user_lost:
         print("GAME OVER!")
     else:
@@ -43,5 +72,3 @@ def play():
 
 if __name__ == "__main__":
     play()
-    
-
